d526922d947263711f17d299e2cb2f9ed8785776
Roy Sivan <roy@guavaworks.com>
2015-12-15 08:25:53 -0800

    init commit

---
+++ json-admin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: JS API ADMIN
+ * Description: A JS SPA App in WP Admin
+ * Author: Roy Sivan
+ * Author URI: http://www.roysivan.com
+ * Version: 0.1
+ * Plugin URI: https://github.com/WP-API/WP-API
+ * License: GPL2+
+ */
+
+
+?>
